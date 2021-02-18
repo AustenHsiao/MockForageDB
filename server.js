941dafa2e2a337536888619220a6006a8cbf7637
AustenHsiao <Austenhsiao@gmail.com>
2021-02-17 22:33:36 -0800

    added templating

--- server.js
+++ server.js
@@ -1,40 +1,45 @@
 const { Client } = require('pg');
 const express = require('express');
 const app = express();
-const path = require('path');
 const router = express.Router();
+const path = require('path');
+const { send } = require('process');
 
 const port = process.env.PORT || 5000;
 
 app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.use("/", express.static(path.join(__dirname, '/pics')));
 
 /*
  Create the client object that will later connect to the db.
  these credentials are taken from the Heroku DB details
+*/
 const client = new Client({
   user: "fvjpwaqeisxmgw",
   password: "d7d3bd8c6e4391078e1cfab9677f27a6b6ba4d6c5c74b05f5c278b8b3fbff231",
   host: "ec2-52-7-168-69.compute-1.amazonaws.com",
   port: 5432,
   database: "ddq07uk2vk6qv6"
-})*/
+})
 
 /* returns promise
  Connect to db Client (capital C) with the object, client. On a successful query,
  print out all of the information (this is just to test and I know the bounds of the data)
  If no connection is made, give the error. In either case, end the connection. We don't want to keep it open.
+*/
 function test_Query() {
   client.connect()
     .then(() => console.log("We're connected"))
     .then(() => client.query("SELECT * FROM user"))
-    .then((result) => console.table(result.rows))
+    .then((result) =>
+      console.table(result.rows)
+    )
     .catch((e) => console.error(e))
     .finally(() => client.end())
-}*/
+}
 
-router.get("/", (req, res) => {
-  res.render("about", { title: "Hey", message: "Hello there!" });
+app.get("/", (req, res) => {
+  res.render("forage", { locations: test_Query() });
 });
 
 app.listen(port, () => {
