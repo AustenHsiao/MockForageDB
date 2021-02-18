7d9917b209f53f62022c75f6aaaf438baa5edb8b
AustenHsiao <Austenhsiao@gmail.com>
2021-02-18 03:04:35 -0800

    trying to fix the client issue

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const path = require('path');
-const pg = require('pg')
-const Client = pg.Client();
-const pool = pg.Pool();
+const { Client } = require('pg')
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
@@ -16,33 +14,20 @@ app.set("view engine", "pug");
 app.use("/", express.static(path.join(__dirname, '/pics')));
 
 app.get("/", (req, res) => {
-  pool.connect((err, client, release) => {
-    client.query("SELECT * FROM location")
-      .then((result) => {
-        console.table(result.rows);
-        res.render("forage", { locations: JSON.stringify(result.rows) });
-      })
-      .catch((e) => {
-        console.log(e);
-        res.render("forage", { locations: "ERROR" });
-      })
-      .finally(() => release())
-  })
-
-
-
-  /*client.connect()
+  client.connect()
     .then(() => console.log("Connected to DB"))
     .then(() => client.query("SELECT * FROM location"))
     .then((result) => {
       console.table(result.rows);
       res.render("forage", { locations: JSON.stringify(result.rows) });
+      client.end();
     })
     .catch((e) => {
       console.log(e);
-      res.render("forage", { locations: "ERROR"});
+      res.render("forage", { locations: "ERROR" });
+      client.end();
     })
-    .finally(() => client.end())*/
+    .finally(() => client.end())
 });
 
 app.listen(port, () => {
