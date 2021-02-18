10ef6a36579d9ecc282bad4bafd4964bd4c3872d
AustenHsiao <Austenhsiao@gmail.com>
2021-02-17 19:47:42 -0800

    test

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const express = require('express');
+const { Client } = require('pg');
 var app = express();
 
 const path = require('path');
 const port = process.env.PORT || 5000;
 
+/*
+ Create the client object that will later connect to the db.
+ these credentials are taken from the Heroku DB details
+*/
+const client = new Client({
+  user: "fvjpwaqeisxmgw",
+  password: "d7d3bd8c6e4391078e1cfab9677f27a6b6ba4d6c5c74b05f5c278b8b3fbff231",
+  host: "ec2-52-7-168-69.compute-1.amazonaws.com",
+  port: 5432,
+  database: "ddq07uk2vk6qv6"
+})
+
+/* returns promise
+ Connect to db Client (capital C) with the object, client. On a successful query,
+ print out all of the information (this is just to test and I know the bounds of the data)
+ If no connection is made, give the error. In either case, end the connection. We don't want to keep it open.
+*/
+function test_Query() {
+  client.connect()
+    .then(() => console.log("We're connected"))
+    .then(() => client.query("SELECT * FROM user"))
+    .then((result) => console.table(result.rows))
+    .catch((e) => console.error(e))
+    .finally(() => client.end())
+}
+
 app.use('/', express.static(__dirname + '/pages'));
 app.use('/', express.static(__dirname + '/css'));
+test_Query();
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.status(200);
+  test_Query();
   res.sendFile(path.join(__dirname + '/pages/index.html'));
-});
+});*/
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
