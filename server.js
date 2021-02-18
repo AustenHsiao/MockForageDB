5cdf9a0a3a10bc074fccad836121566cb94b6d61
AustenHsiao <Austenhsiao@gmail.com>
2021-02-18 03:24:55 -0800

    trying to use pg pool

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const path = require('path');
-const { Client } = require('pg')
-const client = new Client({
+const { Pool } = require('pg');
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
-})
+});
+
+//const { Client } = require('pg')
+/*const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+})*/
 
 const port = process.env.PORT || 5000;
 
@@ -14,7 +20,30 @@ app.set("view engine", "pug");
 app.use("/", express.static(path.join(__dirname, '/pics')));
 
 app.get("/", (req, res) => {
-  client.connect()
+  pool.connect((err, client, release) => {
+    if (err) {
+      return console.error('connexion error', err);
+    }
+    client.query("SELECT * FROM location")
+      .then((result) => {
+        console.table(result.rows);
+        res.render("forage", { locations: JSON.stringify(result.rows) });
+      }).catch((e) => {
+        console.log(e);
+        res.render("forage", { locations: "ERROR" });
+      })
+      .finally(() => release())
+
+  })
+
+
+
+
+
+
+
+
+  /*client.connect()
     .then(() => console.log("Connected to DB"))
     .then(() => client.query("SELECT * FROM location"))
     .then((result) => {
@@ -27,7 +56,7 @@ app.get("/", (req, res) => {
       console.log(e);
       res.render("forage", { locations: "ERROR" });
     })
-    .finally(() => client.end())
+    .finally(() => client.end())*/
 });
 
 app.listen(port, () => {
