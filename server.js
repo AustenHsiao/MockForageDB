1b863e22e92ef764fd04f04fbdf81a4d5d6647a7
AustenHsiao <Austenhsiao@gmail.com>
2021-02-18 13:51:59 -0800

    trying to format query result

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.get("/", (req, res) => {
         //console.log(result.rows.values());
 
         //let location_array = result.rows.values();
+        location_results = result.rows;
+        /*
         (result.rows).forEach(element => {
           console.log(JSON.stringify(element));
           let location_data = JSON.parse(JSON.stringify(element));
@@ -32,31 +34,31 @@ app.get("/", (req, res) => {
           location_results += "\n|" + location_data["lng"];
           location_results += "\n|" + location_data["spotname"];
           location_results += "\n|" + location_data["spotcomment"] + "\n\n";
+*/
+        /*if (location_data.length > 1) {
+          location_results += element + '\n';
+        } else {
+          location_results += '\n';
+        }*/
 
-          /*if (location_data.length > 1) {
-            location_results += element + '\n';
-          } else {
-            location_results += '\n';
-          }*/
-
-          /*
-          let loc = JSON.parse(element);
-          location_results += loc["lat"];
-          location_results += "\n" + loc["lng"];
-          location_results += "\n" + loc["id"];
-          location_results += "\n" + loc["spotname"];
-          location_results += "\n" + loc["spotcomment"] + "\n\n";*/
-        })
-      }).catch((e) => {
-        console.log(e);
-        location_results = "ERROR";
+        /*
+        let loc = JSON.parse(element);
+        location_results += loc["lat"];
+        location_results += "\n" + loc["lng"];
+        location_results += "\n" + loc["id"];
+        location_results += "\n" + loc["spotname"];
+        location_results += "\n" + loc["spotcomment"] + "\n\n";*/
       })
-      .finally(() => {
-        release();
-
-        res.render("forage", { locations: location_results })
-      })
+  }).catch((e) => {
+    console.log(e);
+    location_results = "ERROR";
   })
+    .finally(() => {
+      release();
+
+      res.render("forage", { locations: location_results })
+    })
+})
 });
 
 app.listen(port, () => {
