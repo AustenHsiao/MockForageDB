1ef8b04b562244a763c766c77358c80c9cf11fdc
AustenHsiao <Austenhsiao@gmail.com>
2021-02-18 21:42:03 -0800

    filter function test

--- views/search.js
+++ views/search.js
@@ -1,3 +1,10 @@
-function location_search() {
+function user_search() {
+    let search_parameter = document.getElementById("searchBar");
+    let locations = document.getElementsByClassName("userid");
+    locations.forEach(loc => {
+        if (search_parameter === loc.innerHTML) {
+            loc.style.display = "block";
+        }
+    });
 
 }
