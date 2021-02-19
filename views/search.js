deb60da94fef6803d33e0bafaa31c24a93336af8
AustenHsiao <Austenhsiao@gmail.com>
2021-02-18 22:30:41 -0800

    Changed logic in search function

--- views/search.js
+++ views/search.js
@@ -1,9 +1,10 @@
 function user_search() {
     let search_parameter = document.getElementById("userNameInput").value;
-    let locations = document.getElementsByClassName("userid");
+    let locations = document.getElementsByClassName("location_data");
 
     for (const loc of locations) {
-        if (String(search_parameter) === loc.innerHTML) {
+        //loc.rows[0].innerText.trim() -- row 0 contains the userid. Trim whitespace
+        if (String(search_parameter) === loc.rows[0].innerText.trim()) {
             loc.style.display = "block";
         }
     };
